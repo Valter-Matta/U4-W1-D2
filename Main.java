5af0f94a18b02e4537c9008dde6d7d293a0dde72
Valter Matta <mattavalter98@gmail.com>
2025-01-08 14:36:09 +0100

    finish

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
    public static void main(String[] args) {
       // es#1 printati
-      // System.out.println(stringaPariDispari("Valter"));
-      // System.out.println(annoBisestile(2016));
+      System.out.println(stringaPariDispari("Valter"));
+      System.out.println(annoBisestile(2016));
 
       // es#2
 
@@ -16,22 +16,25 @@ public class Main {
 
       // es#3
 
-      // System.out.println("Stampo sempre finchè non premi :q ");
-      // String letter = scanner.nextLine();
-      // spelling(letter, scanner);
-      scanner.close();
+      System.out.println("Stampo sempre finchè non premi :q ");
+      String letter = scanner.next();
+      spelling(letter, scanner);
 
+      // ex#4
+      System.out.println("Da quale numero vuoi far partire il razzo?");
+      int num = scanner.nextInt();
+      counter(num);
+      scanner.close();
    }
 
    // es#1
-   // public static boolean stringaPariDispari(String str) {
-   // return (str.length() % 2 == 0) ? true : false;
-   // }
+   public static boolean stringaPariDispari(String str) {
+      return (str.length() % 2 == 0) ? true : false;
+   }
 
-   // public static boolean annoBisestile(int year) {
-   // return (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)) ? true :
-   // false;
-   // }
+   public static boolean annoBisestile(int year) {
+      return (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)) ? true : false;
+   }
 
    // es#2
    public static String costruttoSwitch(int number) {
@@ -54,14 +57,22 @@ public class Main {
       }
    }
 
-   // // es#4
-   // public static void spelling(String str, Scanner scanner) {
+   // es#3
+   public static void spelling(String str, Scanner scanner) {
 
-   // while (!str.equals("q")) {
-   // System.out.println(str);
-   // str = scanner.nextLine();
-   // }
-   // System.out.println("Hai digitato :q , bravo");
+      while (!str.equals("q")) {
+         System.out.println(str);
+         str = scanner.nextLine();
+      }
+      System.out.println("Hai digitato :q , bravo");
 
-   // }
+   }
+
+   // es#4
+   public static void counter(int numb) {
+
+      for (int i = numb; i >= 0; i--) {
+         System.out.println(i);
+      }
+   }
 }
